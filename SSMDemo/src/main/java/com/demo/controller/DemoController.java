b493a2b8d2bbad1588c8faa500862319fc5f8a74
LiuYuanBino <1140180992@qq.com>
2019-07-15 14:56:45 +0800

    add adasdad

--- SSMDemo/src/main/java/com/demo/controller/DemoController.java
+++ SSMDemo/src/main/java/com/demo/controller/DemoController.java
@@ -1,8 +1,8 @@
 package com.demo.controller;
 
 public class DemoController {
-  
-      public static void main(String[] args){
-          system.out.print("hello");
-      }
+
+	public static void main(String[] args) {
+		System.out.println("asdasdasd");
+	}
 }
